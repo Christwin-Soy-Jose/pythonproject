c291d458fdc702b8b45af53e77c0e45e02788b14
ellen4555 <ellenjoy2028@cs.sjcetpalai.ac.in>
2024-11-16 14:08:08 +0530

    Add files via upload

---
+++ ellen/bmi.py
@@ -0,0 +1,12 @@
+weight=float(input("Enter your weight in kilograms:"))
+height=float(input("Enter your height in meters:"))
+BMI=weight/(height**2)
+print("Your BMI is:",BMI)
+if BMI < 18.5:
+    print("You are underweight")
+elif 18.5 <= BMI < 24.9:
+    print("You have a normal weight")
+elif 25 <= BMI < 29.9:
+    print("You are overweight")
+else:
+    print("You have obesity")
