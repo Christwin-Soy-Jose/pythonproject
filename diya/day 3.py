79f5e96c49bfb9c6b4706132a89cba534bdb66b5
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2024-11-28 22:16:55 +0530

    Add files via upload

---
+++ diya/day 3.py
@@ -0,0 +1,8 @@
+'''author :Diya krishna
+version 1.1
+date:28/11/2024'''
+import math
+base=float(input("Enter the base of a triangle : "))
+altitude=float(input("Enter the altitude of the triangle:"))
+hypo=math.sqrt(((base)**2)+((altitude)**2))
+print("hypotenuse of the triangle is ",hypo)
