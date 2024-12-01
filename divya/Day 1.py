e2a0baba2442e0398f2c9b8be4edd9bb57c60678
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2024-12-01 10:45:52 +0530

    Add files via upload

---
+++ divya/Day 1.py
@@ -0,0 +1,11 @@
+'''Author:Divya Kuriakose
+Date:16-11-2024
+program to find the hypotenus using pythagorus theorem
+'''
+
+import math
+base=int(input("Enter the base:"))
+altitude=int(input("Enter the altitude:"))
+hypotenuse=math.sqrt(base*base + altitude*altitude)
+print(f"The hypotenuse of the given right angled triangle is: {hypotenuse}")
+print(f"The hypotenuse of the given right angled triangle is: {hypotenuse}")
