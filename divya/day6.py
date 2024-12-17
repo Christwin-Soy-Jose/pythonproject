9cf24f7ae5af6b463680a7b9ae60f56dc0c15234
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2024-12-17 21:28:57 +0530

    Add files via upload

---
+++ divya/day6.py
@@ -0,0 +1,12 @@
+'''Author:Divya Kuriakose
+Date:16-11-2024
+program to get phone number and insert into list
+'''
+phone_numbers=[]
+n = int(input("Enter how many phone numbers do you want? "))
+for i in range(n):
+    phone_number = input(f"Enter phone number {i + 1}:")
+    phone_numbers.append(phone_number)
+print("Phone numbers entered:")
+for number in phone_numbers:
+   print(number)
