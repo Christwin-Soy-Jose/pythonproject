36666286eb8176aa74fe8f7138fe57bdcc389cd4
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2024-12-17 21:18:43 +0530

    Add files via upload

---
+++ divya/day5.py
@@ -0,0 +1,10 @@
+'''Author:Divya Kuriakose
+Date:16-11-2024
+program to check whether the number is odd or even
+'''
+num=int(input("Enter a number:"))
+mod=num % 2
+if mod> 0:
+    print("The number is an odd number")
+else:
+    print("The number is an even number")
